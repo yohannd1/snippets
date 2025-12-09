c313983fbfbcb11044bb867694a1ec37340cdb74
yohannd1 <mitonanan12@gmail.com>
2025-12-09 13:06:10 -0300

    improving 0006-genetic_minimum

--- 0006-genetic_minimum/main.py
+++ 0006-genetic_minimum/main.py
@@ -6,22 +6,55 @@ import math
 import abc
 from random import random
 from math import cos, sin, radians, atan2
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Sequence, Callable
 from dataclasses import dataclass
 
-from pygame.locals import *
+@dataclass
+class Point2:
+    x: float
+    y: float
 
-from point import Point2
+    def __truediv__(self, rhs):
+        if isinstance(rhs, Point2):
+            return Point2(
+                self.x / rhs.x,
+                self.y / rhs.y,
+            )
+        elif isinstance(rhs, (int, float)):
+            return Point2(
+                self.x / rhs,
+                self.y / rhs,
+            )
+        else:
+            raise ValueError(
+                f"Don't know how to divide Point2({type(self.x)}, {type(self.y)}) by {type(rhs)}"
+            )
 
-SCREEN_SIZE = Point2(1200, 700)
-SCREEN_CENTER = SCREEN_SIZE / 2
+    def __add__(self, rhs):
+        if isinstance(rhs, Point2):
+            return Point2(
+                self.x + rhs.x,
+                self.y + rhs.y,
+            )
+        elif isinstance(rhs, (int, float)):
+            return Point2(
+                self.x + rhs,
+                self.y + rhs,
+            )
+        else:
+            raise ValueError(
+                f"Don't know how to add Point2({type(self.x)}, {type(self.y)}) with {type(rhs)}"
+            )
 
-FRAMERATE = 75
-SECONDS_PER_FRAME = 1.0 / FRAMERATE
+    def copy(self) -> Point2:
+        return Point2(self.x, self.y)
+
+    def to_tup(self):
+        return (self.x, self.y)
 
 
 @dataclass
-class Color:  # FIXME: this is trash
+class Color:
     r: int
     g: int
     b: int
@@ -30,36 +63,32 @@ class Color:  # FIXME: this is trash
         return (self.r, self.g, self.b)
 
 
-WHITE = Color(255, 255, 255)
-BLACK = Color(0, 0, 0)
+SCREEN_SIZE = Point2(1200, 700)  # o tamanho da tela
+SCREEN_CENTER = SCREEN_SIZE / 2  # coordenadas do meio da tela
+
+FRAMERATE = 20  # taxa de atualização (Hz)
+FRAME_PERIOD = 1.0 / FRAMERATE  # período de um frame (sec)
+
+BG_COLOR = Color(30, 30, 25)
+LINE_COLOR = Color(128, 128, 128)
+INDIV_COLOR = Color(255, 0, 0)
+TEXT_COLOR = Color(200, 200, 200)
+
+POPL_SIZE = 100  # tamanho da população
+X_INTERVAL = (0, 50)  # intervalo de busca do mínimo
+
+# função de busca
+FUNCTION = lambda x: 0.02 * x**2 - 0.8 * x + cos(x) + 10
 
 
-def main():
+def main() -> None:
+    pygame.font.init()
     canvas = PygameCanvas(surface=pygame.display.set_mode(SCREEN_SIZE.to_tup()))
 
-    global_timer = 0
-    bullets = []
-
-    def make_func_points(
-        func, interval: tuple[float, float], n: int
-    ) -> tuple[list[float], list[float]]:
-        x0, xf = interval
-        h = (xf - x0) / n
-        xs = [x0 + i * h for i in range(n + 1)]
-        ys = [func(x) for x in xs]
-        return xs, ys
-
-    FUNC = lambda x: 0.02 * x**2 - 0.8 * x + cos(x) + 10
-    x_intv = (0, 50)
-    xs, ys = make_func_points(FUNC, x_intv, int(SCREEN_SIZE.x / 3))
-
-    GEN_SIZE = 100
-
-    generation = [x_intv[0] + x_intv[1] * random() for _ in range(GEN_SIZE)]
+    mf = MinimumFinder()
 
     running = True
     while running:
-        global_angle = radians(global_timer)
         frame_start = time.time()
         next_generation = False
 
@@ -70,55 +99,16 @@ def main():
                 if event.key == ord("n"):
                     next_generation = True
 
-        canvas.fill(BLACK)
+        mf.process()
 
-        COLOR = Color(128, 128, 128)
-
-        min_x, max_x = xs[0], xs[-1]
-        amp_x = max_x - min_x
-
-        min_y, max_y = min(ys), max(ys)
-
-        amp_y = max_y - min_y
-        min_y -= amp_y * 0.075
-        max_y += amp_y * 0.075
-
-        # actual points upon rendering
-        phys_points = [
-            Point2(
-                (x - min_x) / amp_x * SCREEN_SIZE.x,
-                SCREEN_SIZE.y - (y - min_y) / amp_y * SCREEN_SIZE.y,
-            )
-            for (x, y) in zip(xs, ys)
-        ]
-        canvas.draw_lines(phys_points, 2, COLOR)
-
-        for x in generation:
-            x_phys = (x - min_x) / amp_x * SCREEN_SIZE.x
-            y_phys = SCREEN_SIZE.y - (FUNC(x) - min_y) / amp_y * SCREEN_SIZE.y
-            canvas.draw_rect_centered(
-                Point2(x_phys, y_phys), Point2(5, 5), Color(255, 0, 0)
-            )
+        canvas.fill(BG_COLOR)
+        mf.draw_to(canvas)
 
         canvas.update()
 
+        # isso aqui é necessário para garantir a taxa de atualização correta
         elapsed = time.time() - frame_start
-        time.sleep(max(0, SECONDS_PER_FRAME - elapsed))
-
-        global_timer += 1
-
-        if True:
-            g_ys = [FUNC(x) for x in generation]
-            g_ws = [max_y - FUNC(x) for x in generation]  # weights
-            new_child = sum(x * w for (x, w) in zip(generation, g_ws)) / sum(g_ws)
-            generation.append(new_child)
-
-            paired = list(zip(generation, g_ys))
-            sorted_by_rank = sorted(paired, key=lambda a: a[1])
-            generation.remove(sorted_by_rank[-1][0])
-            generation.remove(sorted_by_rank[-2][0])
-
-            generation.append(x_intv[0] + x_intv[1] * random())
+        time.sleep(max(0, FRAME_PERIOD - elapsed))
 
     pygame.quit()
 
@@ -158,10 +148,10 @@ class Canvas:
 class PygameCanvas(Canvas):
     surface: pygame.Surface
 
-    def fill(self, color: Color):
+    def fill(self, color: Color) -> None:
         self.surface.fill((color.r, color.g, color.b))
 
-    def draw_rect(self, top_left: Point2, size: Point2, color: Color):
+    def draw_rect(self, top_left: Point2, size: Point2, color: Color) -> None:
         pygame.draw.rect(
             self.surface,
             (color.r, color.g, color.b),
@@ -171,17 +161,21 @@ class PygameCanvas(Canvas):
             ),
         )
 
-    def draw_line(self, start: Point2, end: Point2, thickness: float, color: Color):
+    def draw_line(
+        self, start: Point2, end: Point2, thickness: float, color: Color
+    ) -> None:
         pygame.draw.line(
             self.surface,
             (color.r, color.g, color.b),
             start.to_tup(),
             end.to_tup(),
-            thickness,
+            int(thickness),
         )
 
-    @abc.abstractmethod
-    def update(self):
+    def draw_surface(self, surface: pygame.Surface, pos: Point2) -> None:
+        self.surface.blit(surface, pos.to_tup())
+
+    def update(self) -> None:
         pygame.display.update()
 
 
@@ -190,86 +184,103 @@ class BaseEntity:
     position: Point2
 
     @abc.abstractmethod
-    def should_delete(self) -> bool: ...
+    def process(self) -> None: ...
 
     @abc.abstractmethod
-    def process(self): ...
-
-    @abc.abstractmethod
-    def draw_to(self, canvas: Canvas): ...
+    def draw_to(self, canvas: Canvas) -> None: ...
 
 
-@dataclass
-class SimpleBullet(BaseEntity):
-    speed: Point2
-    size: Point2
-
-    def should_delete(self) -> bool:
-        return self.is_out_of_screen(SCREEN_SIZE)  # FIXME: haha global variable
-
-    def is_out_of_screen(self, screen_dimensions: Point2) -> bool:
-        pos = self.position
-        size = self.size
-        scr = screen_dimensions
-
-        return (
-            pos.x + size.x < 0 or scr.x < pos.x or pos.y + size.y < 0 or scr.y < pos.y
+class MinimumFinder(BaseEntity):
+    def __init__(self) -> None:
+        xs, ys = MinimumFinder._calc_function_points(
+            func=FUNCTION,
+            interval=X_INTERVAL,
+            n=int(SCREEN_SIZE.x / 3), # escolhendo uma resolução baseada no tamanho da tela
         )
 
-    def process(self):
-        self.position += self.speed
+        self.func_xs, self.func_ys = xs, ys
 
-    def draw_to(self, canvas: Canvas):
-        canvas.draw_rect_centered(self.position, self.size, Color(150, 180, 120))
+        self.min_x, self.max_x = xs[0], xs[-1]
+        self.amp_x = self.max_x - self.min_x
 
+        self.min_y, self.max_y = min(ys), max(ys)
+        self.amp_y = self.max_y - self.min_y
 
-@dataclass
-class OscillatingSpeedBullet(BaseEntity):
-    angle: float
-    size: Point2
-    speed_minmax: Tuple[float, float]
-    timer: int = 0
-    timer_inc: int = 1
-    color: Optional[Color] = None
+        # ampliar um pouco mais o intervalo y para facilitar visualização
+        MAGNIFY_RATE = 0.075
+        self.min_y -= self.amp_y * MAGNIFY_RATE
+        self.max_y += self.amp_y * MAGNIFY_RATE
 
-    def __post_init__(self):
-        mn, mx = self.speed_minmax
-        if mn > mx:
-            raise ValueError(
-                f"Minimal value {mn} (first) should be smaller or equal to maximum value {mx} (second)"
+        # criar a população inicial - ela é um conjunto de pontos x aleatórios
+        self.population = [MinimumFinder._make_random_x() for _ in range(POPL_SIZE)]
+
+        self.font = pygame.font.SysFont("Arial", 20)
+        self.best: Optional[Point2] = None
+
+    @staticmethod
+    def _calc_function_points(
+        func: Callable[[float], float],
+        interval: tuple[float, float],
+        n: int,
+    ) -> tuple[list[float], list[float]]:
+        """Calcula os pontos da função `func` no intervalo especificado. Retorna xs, ys."""
+
+        x0, xf = interval
+        h = (xf - x0) / n
+        xs = [x0 + i * h for i in range(n + 1)]
+        ys = [func(x) for x in xs]
+        return xs, ys
+
+    @staticmethod
+    def _make_random_x() -> float:
+        """Criar um indivíduo novo aleatório."""
+        xmin, xmax = X_INTERVAL
+        return xmin + xmax * random()
+
+    def process(self) -> None:
+        xs = self.population
+        ys = [FUNCTION(x) for x in xs]
+
+        # avaliar cada membro através de um peso (weight), que é só a distância
+        # do valor y para o y máximo - quão mais em baixo, maior o peso.
+        ws = [self.max_y - y for (x, y) in zip(xs, ys)]
+
+        # criar um novo indivíduo através da média ponderada das posições x e
+        # seus pesos
+        new_x = sum(x * w for (x, w) in zip(xs, ws)) / sum(ws)
+        self.population.append(new_x)
+
+        # adicionar uma mutação à população (novo ponto aleatório)
+        self.population.append(MinimumFinder._make_random_x())
+
+        # fazer uma lista ordenada da população, de acordo com seus valores y, e remover o pior
+        sorted_by_rank = sorted(zip(xs, ys), key=lambda a: a[1])
+        for (x, _) in sorted_by_rank[-2:]:
+            self.population.remove(x)
+
+        best_x, best_y = sorted_by_rank[0]
+        self.best = Point2(best_x, best_y)
+
+    def draw_to(self, canvas: Canvas) -> None:
+        # desenhar gráfico da função (traduzindo coordenadas da função para
+        # coordenadas da tela)
+        phys_points = [
+            Point2(
+                (x - self.min_x) / self.amp_x * SCREEN_SIZE.x,
+                SCREEN_SIZE.y - (y - self.min_y) / self.amp_y * SCREEN_SIZE.y,
             )
+            for (x, y) in zip(self.func_xs, self.func_ys)
+        ]
+        canvas.draw_lines(phys_points, 2, LINE_COLOR)
 
-    def should_delete(self) -> bool:
-        return self.is_out_of_screen(SCREEN_SIZE)
+        # desenhando pontos da população
+        for x in self.population:
+            x_phys = (x - self.min_x) / self.amp_x * SCREEN_SIZE.x
+            y_phys = SCREEN_SIZE.y - (FUNCTION(x) - self.min_y) / self.amp_y * SCREEN_SIZE.y
+            canvas.draw_rect_centered(Point2(x_phys, y_phys), Point2(5, 5), INDIV_COLOR)
 
-    def is_out_of_screen(self, screen_dimensions: Point2) -> bool:
-        pos = self.position
-        size = self.size
-        scr = screen_dimensions
-
-        return (
-            pos.x + size.x < 0 or scr.x < pos.x or pos.y + size.y < 0 or scr.y < pos.y
-        )
-
-    def process(self):
-        intensity = cos(radians(self.timer))
-
-        mn, mx = self.speed_minmax
-        # middle = (mx - mn) / 2
-        speed = mn + (mx - mn) * max(0, intensity)
-
-        self.position += Point2(
-            speed * cos(self.angle),
-            -speed * sin(self.angle),
-        )
-
-        self.timer += self.timer_inc
-
-    def draw_to(self, canvas: Canvas):
-        canvas.draw_rect_centered(
-            self.position, self.size, self.color or Color(150, 180, 120)
-        )
-
+        text_surface = self.font.render(f"Mínimo atual: {self.best}", False, TEXT_COLOR.to_tup())
+        canvas.draw_surface(text_surface, Point2(0, 0))
 
 if __name__ == "__main__":
     main()
